f3e939438b1793b699bd021c6a2afa0ff2b5d870
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-07-23 13:13:28 +0700

    update

--- app/components/WelcomeBanner.jsx
+++ app/components/WelcomeBanner.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { currentUser } from '@clerk/nextjs'
+import React from "react";
+import { currentUser } from "@clerk/nextjs";
 
 async function WelcomeBanner() {
-
   const user = await currentUser();
 
   return (
     <div className="flex gap-5 items-center bg-white dark:bg-slate-900 rounded-md p-5">
-        <div>
-          <h2 className="text-[30px] font-bold mb-4">Selamat Datang <span className="text-primary">{user.firstName} {user.lastName}</span></h2>
-          <p>Mari Belajar Pemrograman Web di <span className="text-primary">Webcademy</span> Bersama Para Sepuh 😃😁</p>
-        </div>
+      <div>
+        <h2 className="text-[30px] font-bold mb-4">
+          Selamat Datang{" "}
+          <span className="text-primary">
+            {user.firstName} {user.lastName}
+          </span>
+        </h2>
+        <p>
+          Mari belajar pemrograman sampai mahir di{" "}
+          <span className="text-primary">IEGWEBCADEMY</span> bersama mentor
+          terbaik.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default WelcomeBanner
+export default WelcomeBanner;
